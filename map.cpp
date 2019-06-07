7d797bf1edf9283e095314b1d139fcc5a10f193e
Stephano <stephano.wurttele@utec.edu.pe>
2019-06-07 13:23:27 -0500

    3

---
+++ map.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+class Seccion{
+	public:
+	vector<map> students;
+	Seccion():{};
+	add(string name,string age){
+		students.push_back(map[name]=age);
+	}
+	borrar(string name,string age){
+		students.pop_back(map[name]=age);
+	}
+};
+
+int main(){
+	string="C Jacinto 10";
+	string read=x.split(" ");
+	if(x[0])
+
+
+
